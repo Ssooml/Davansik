c96094f3d5c64ab810e463cf0188b39ebc06125a
Ssooml <luwka0@gmail.com>
2024-12-09 05:47:49 +0100

    Update routes.py

--- webapp/routes.py
+++ webapp/routes.py
@@ -1,7 +1,18 @@
-from flask import Blueprint, render_template
+from flask import render_template_string
 
-webapp_bp = Blueprint('webapp', __name__, template_folder='templates')
-
-@webapp_bp.route('/webapp/<username>', methods=['GET'])
-def webapp(username):
-    return render_template('webapp.html', username=username)
+def configure_routes(app):
+    @app.route('/webapp/<username>', methods=['GET'])
+    def webapp(username):
+        return render_template_string("""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Web App</title>
+        </head>
+        <body>
+            <h1>Привет, {{ username }}</h1>
+        </body>
+        </html>
+        """, username=username)
