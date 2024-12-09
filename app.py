b7ae929c880da45807ba4fd636886449800ad99c
Ssooml <luwka0@gmail.com>
2024-12-09 05:49:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
-from threading import Thread
-from bot import bot
 from webapp import create_app
+from bot import run_bot
+from threading import Thread
 
-# --- Настройки ---
-PORT = 5000
+# Создаем экземпляр приложения Flask
+app = create_app()
 
 if __name__ == "__main__":
-    # Создание Flask-приложения
-    app = create_app()
-
-    # Запуск Flask-приложения в отдельном потоке
-    Thread(target=app.run, kwargs={'host': '0.0.0.0', 'port': PORT}).start()
-
-    # Запуск бота
-    bot.polling(non_stop=True)
+    # Запускаем Flask и бота в отдельных потоках
+    flask_thread = Thread(target=lambda: app.run(host="0.0.0.0", port=5000))
+    bot_thread = Thread(target=run_bot)
+    flask_thread.start()
+    bot_thread.start()
+    flask_thread.join()
+    bot_thread.join()
